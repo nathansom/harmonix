517120ccf578e3e33abdab72346ae6ab6a612868
David Baron <94808313+H0RSESH0E@users.noreply.github.com>
2022-10-05 18:41:25 -0400

    Update Startup.cs

--- API/Startup.cs
+++ API/Startup.cs
@@ -21,7 +21,7 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(options=>{
-                options.UseNpgsql(_config.GetConnectionString("POSTGRESQLCONNSTR_DefaultConnection"));
+                options.UseNpgsql(_config.GetConnectionString("DefaultConnection"));
             });
             services.AddApplicationServices(_config);
             services.AddControllers();
